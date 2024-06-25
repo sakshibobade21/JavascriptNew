7afd43a47b4826218c37f2b518ce4cdafcf8b97d
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-06-25 16:51:17 +0530

    use reduce

---
+++ methods/op.js
@@ -0,0 +1,37 @@
+const users = [
+  {firstName: 'Sakshi', lastName: 'B' , age: 27 },
+  {firstName: 'Alex', lastName: 'D' , age: 16 },
+  {firstName: 'Phil', lastName: 'Dun' , age: 50 },
+  {firstName: 'Halley', lastName: 'Phy' , age: 27 },
+];
+
+// {27: 2, 16: 1, 50:1}
+
+const reduce = users.reduce((acc, curr) => {
+  if(acc[curr.age]) {
+    acc[curr.age] += 1;
+  } else {
+    acc[curr.age] = 1;
+  }
+  return acc;
+}, {});
+
+const red = users.reduce((acc, curr) => {
+  acc[curr.age] = acc[curr.age] ? acc[curr.age] + 1: 1;
+  return acc;
+}, {});
+
+const obj = {
+  Sakshi: 'Hey',
+  age: 20
+}
+
+console.log(obj.Sakshi);
+console.log(obj[users[0].firstName]);
+
+obj.age = obj.age+10;
+
+console.log(obj);
+
+console.log(reduce);
+console.log(red);
