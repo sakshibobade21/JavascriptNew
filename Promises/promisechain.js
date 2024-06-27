cb6bb61370bf73a22d421fa8da60ed1c37f5c582
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-06-27 18:19:14 +0530

    Promise chaing

---
+++ Promises/promisechain.js
@@ -0,0 +1,25 @@
+const fetchData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Data is fetched');
+    }, 1000)
+  })
+}
+
+const updateData = (data) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data.toUpperCase());
+    }, 1000)
+  })
+}
+
+fetchData()
+  .then(resp => {
+    console.log(resp);
+    return updateData(resp);
+  })
+  .then(resp => {
+    console.log(resp);
+  })
+  .catch(err => console.log(err))
