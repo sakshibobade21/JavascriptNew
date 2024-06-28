dc016635f67db8b969738daf3f7103f14b85c6a9
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-06-28 13:09:29 +0530

    promise all

---
+++ Promises/methods/all.js
@@ -0,0 +1,18 @@
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("resolved after 1 seconds");
+    }, 1000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("resolved after 2 seconds");
+  }, 2000)
+});
+
+const promise3 = Promise.resolve("resolved immediately");
+
+Promise.all([promise2, promise1,promise3])
+  .then(values => {
+    console.log(values);
+  })
